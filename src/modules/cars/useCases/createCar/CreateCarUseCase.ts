ef9c884be78e55b6346be114c7936ec3e06a7c50
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-02-04 19:23:51 -0300

    Criacao de testes para Carro utilizando TDD

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,54 @@
+import { inject, injectable } from "tsyringe";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { AppError } from "@shared/errors/AppError";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+interface IRequest {
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  find_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+//@injectable()
+class CreateCarUseCase {
+  constructor(
+    //   @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) { }
+
+  async execute({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    find_amount,
+    brand,
+    category_id }: IRequest): Promise<Car> {
+
+    const carAlreadyExist = await this.carsRepository.findByLicencePlate(
+      license_plate
+    );
+
+    if (carAlreadyExist) {
+      throw new AppError("Car already exist!");
+    }
+
+    const car = await this.carsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      find_amount,
+      brand,
+      category_id
+    });
+
+    return car;
+  }
+}
+
+export { CreateCarUseCase }
