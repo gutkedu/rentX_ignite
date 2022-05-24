507d3f15203a0eda7da567ad5f0b3bbf90094dc9
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-05-24 16:15:56 -0300

    fix: createRental car available

--- README.md
+++ README.md
@@ -51,3 +51,18 @@ O aluguel deve ter duração mínima de 24 horas.
 Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
 Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
 O usuario deve estar logado na aplicação
+Ao realizar um aluguel, o status do carro devera ser alterado para indisponivel
+
+# Devolução de um carro
+
+**RF**
+Deve ser possivel realizar a devolução de um carro
+
+**RN**
+Se o carro for devolvido com menos de 24 horas, deve ser cobrado diaria completa
+Ao realizar a devolução, o carro deverá ser liberado para outro aluguel
+Ao realizar a devolução, o usuario devera ser liberado para outro aluguel
+Ao realizar a devolução, deverá ser calculado o total do aluguel
+Caso o horario de devolução seja superior ao horário previsto de entrega, deverá ser cobrado
+multa proporcional aos dias de atraso
+Caso haja multa, deverá ser somado ao total do aluguel
--- src/modules/rentals/useCases/createRental/createRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/createRentalUseCase.spec.ts
@@ -4,19 +4,23 @@ import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memo
 import { AppError } from "@shared/errors/AppError";
 import { CreateRentalUseCase } from "./createRentalUseCase";
 import { DayJsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let dayJsDateProvider: DayJsDateProvider;
 
 describe("Create Rental", () => {
   const dayAdd24Hours = dayjs().add(1, "day").toDate();
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
     dayJsDateProvider = new DayJsDateProvider();
     createRentalUseCase = new CreateRentalUseCase(
       rentalsRepositoryInMemory,
-      dayJsDateProvider
+      dayJsDateProvider,
+      carsRepositoryInMemory
     );
   });
 
