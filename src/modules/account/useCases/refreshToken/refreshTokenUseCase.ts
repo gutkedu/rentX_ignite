b234e634185966e9f6121154c085fe67628a0191
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-07-22 11:12:51 -0300

    🐞 fix: refresh token logic

--- src/modules/account/useCases/refreshToken/refreshTokenUseCase.ts
+++ src/modules/account/useCases/refreshToken/refreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
 @injectable()
 export class RefreshTokenUseCase {
   constructor(
@@ -19,7 +24,7 @@ export class RefreshTokenUseCase {
     private dateProvider: IDateProvider
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
 
     const user_id = sub;
@@ -51,7 +56,12 @@ export class RefreshTokenUseCase {
       user_id,
     });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return { token: newToken, refresh_token };
   }
 }
 
